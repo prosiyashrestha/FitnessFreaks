ff1f6eb05df4881e64e7660bec28b7ad17034bc8
prosiyashrestha <prosiyashrestha@gmail.com>
2023-08-10 00:23:45 +0545

    file add

---
+++ src/main/java/com/system/fitnessfreaks/controller/CategoryController.java
@@ -0,0 +1,12 @@
+package com.system.fitnessfreaks.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping()
+public class CategoryController {
+
+}
