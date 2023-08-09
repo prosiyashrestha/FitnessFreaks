3f1e85adff368f9703459e15a9061f9f1fcbfe1b
prosiyashrestha <prosiyashrestha@gmail.com>
2023-08-10 00:02:11 +0545

    first push

---
+++ src/main/java/com/system/fitnessfreaks/FitnessFreaksApplication.java
@@ -0,0 +1,13 @@
+package com.system.fitnessfreaks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessFreaksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessFreaksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/system/fitnessfreaks/FitnessFreaksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.fitnessfreaks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessFreaksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
