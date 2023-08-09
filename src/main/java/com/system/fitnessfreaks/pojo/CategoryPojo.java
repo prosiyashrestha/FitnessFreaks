cec9881be40a9018e05ad2365ffa88395a753db0
prosiyashrestha <prosiyashrestha@gmail.com>
2023-08-10 00:37:00 +0545

    add

---
+++ src/main/java/com/system/fitnessfreaks/pojo/CategoryPojo.java
@@ -0,0 +1,27 @@
+//package com.system.fitnessfreaks.pojo;
+//
+//import com.system.fitnessfreaks.Entity.Admin;
+//import com.system.fitnessfreaks.Entity.Category;
+//import com.system.fitnessfreaks.Entity.Membership;
+//import com.system.fitnessfreaks.Entity.SignUp;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class CategoryPojo {
+//    private Integer id;
+//    private String category;
+//    private String duration;
+//    private Admin price_id;
+//
+//    public CategoryPojo(Category category) {
+//        this.id= category.getId();
+//        this.category= category.getCategory();
+//        this.price_id= Admin.builder().build();
+//    }
+//}
